5b39fb9df26d5148bfe6b13eff107ea0aa8dd437
phoenix <wengliang_1986@126.com>
2019-01-13 18:18:42 +0800

    Thymeleaf

--- restful-crud/src/main/java/com/my/test/web/restfulcrud/controller/ThymeleafController.java
+++ restful-crud/src/main/java/com/my/test/web/restfulcrud/controller/ThymeleafController.java
@@ -3,14 +3,18 @@ package com.my.test.web.restfulcrud.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class ThymeleafController {
 
     @GetMapping("success")
-    public String success(Map<String, String> params) {
-        params.put("key1", "value1");
+    public String success(Map<String, Object> params) {
+        params.put("hello", "<h2>你好</h2>");
+        List<String> list = Arrays.asList("zhansan", "lisi", "wangwu");
+        params.put("users", list);
         return "success";
     }
 }
